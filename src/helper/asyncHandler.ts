b6bef2771d8357c382eedb5cec42fc0ce34d6b4c
Hadi <969hadiahmed@gmail.com>
2025-06-12 16:04:34 +0530

    feat: asyncHandler

---
+++ src/helper/asyncHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express"
+
+type Controller = (req: Request, res: Response, next: NextFunction) => Promise<void | Response>
+
+const asyncHandler = (fn: Controller) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next))
+        .catch(err => {
+            res.status(500).send({
+                success: false,
+                message: err.message,
+            })
+        })
+}
